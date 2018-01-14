19bdad742e86ea4dfc60095b11e667e8e7a754e1
EMIRATES\S410909 <S410909@emirates.com>
2018-01-14 10:01:56 +0400

    Updated comments

--- src/string/Reverse.java
+++ src/string/Reverse.java
@@ -2,17 +2,20 @@ package string;
 
 public class Reverse {
 	public static void main(String args[]) {
-		// Reverse using the loop
+		
 		String s = "Reverse this String";
 		StringBuffer sBuffer = new StringBuffer(s);
 		StringBuilder sBuild = new StringBuilder(s);
 		String reverse = "";
+		// Reverse using the loop
 		int len = s.length();
 		for (int i = len - 1; i >= 0; i--) {
 			reverse = reverse + s.charAt(i);
 		}
 		System.out.println("Reverse using the loop: "+reverse);
+		// Reverse using the String Buffer
 		System.out.println("Reverse using the String Buffer: "+sBuffer.reverse());
+		// Reverse using the String Builder
 		System.out.println("Reverse using the String Builder: "+sBuild.reverse());
 		
 	}
