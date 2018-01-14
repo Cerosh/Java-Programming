67d258c14b105b364a4b44368abcead985ba4f9f
EMIRATES\S410909 <S410909@emirates.com>
2018-01-14 14:33:47 +0400

    Adding program for reversing integer

--- src/string/ReverseInteger.java
+++ src/string/ReverseInteger.java
@@ -1,21 +1,22 @@
 package string;
 
 public class ReverseInteger {
-	public static void main (String args[]) {
+	public static void main(String args[]) {
 		// Using the logic with arithmetic operators
 		int i = 1234;
 		int reverse = 0;
-		while (i!=0) {
-		reverse = reverse*10 + i % 10;
-		i = i /10;
+		while (i != 0) {
+			reverse = reverse * 10 + i % 10;
+			i = i / 10;
 		}
-		System.out.println("Reverse using arithmetic Operators: "+ reverse);
-		
+		System.out.println("Reverse using arithmetic Operators: " + reverse);
+
 		// Using built in method
 		int i1 = 12345;
 		StringBuilder sb = new StringBuilder(String.valueOf(i1));
 		StringBuilder reverseSB = sb.reverse();
-		System.out.println("Reverse using build in method: "+reverseSB);
+		System.out.println("Reverse using build in method: " + reverseSB);
+
 	}
 
 }
