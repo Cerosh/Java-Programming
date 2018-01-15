21aa05f6e1b1af684625a6b7fdfc60598be6f0f7
Cerosh Jacob <cerosh.jacob@emirates.com>
2018-01-15 11:51:27 +0400

    Array Compare program added

---
+++ src/array/ArrayCompare.java
@@ -0,0 +1,14 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArrayCompare {
+public static void main (String args[]) {
+	String [] One = {"Apple","Mango","Banana"};
+	String [] Two = {"Banana", "Mango","Apple1"};
+	Arrays.sort(One);
+	Arrays.sort(Two);
+	System.out.println(Arrays.equals(One, Two));
+	
+}
+}
